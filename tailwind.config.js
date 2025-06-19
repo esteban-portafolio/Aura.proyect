8e483e073b8c94b551c69b78525a404aa27d980e
estaban-portafolio <e.mendoza.g93@gmail.com>
2025-06-19 16:11:17 -0400

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./**/*.php",    // Escanear todos los archivos PHP
+    "./*.php"        // Incluye archivos en raíz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
